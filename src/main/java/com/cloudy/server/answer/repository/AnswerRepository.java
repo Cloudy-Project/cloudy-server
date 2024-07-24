f55a16cfae14068eaa448cec20c001f6bc5d77ee
ywonchae1 <ywonchae62@gmail.com>
2024-07-24 21:15:19 +0900

    feat: 답장 CRUD 구현 (#10)

--- src/main/java/com/cloudy/server/answer/repository/AnswerRepository.java
+++ src/main/java/com/cloudy/server/answer/repository/AnswerRepository.java
@@ -1,10 +1,16 @@
 package com.cloudy.server.answer.repository;
 
 import com.cloudy.server.answer.domain.Answer;
-import com.cloudy.server.letter.domain.Letter;
+import com.cloudy.server.answer.dto.request.AnswerUpdateRequest;
 
 import java.util.Optional;
 
 public interface AnswerRepository {
     Optional<Answer> findByLetterId(Long letterId);
+
+    Answer save(Answer answer);
+
+    Optional<Answer> findById(Long id);
+
+    Optional<Answer> update(AnswerUpdateRequest answerUpdateRequest);
 }
