68ff85e461e46aacebdc29bb5686da3f153baadf
ywonchae1 <ywonchae62@gmail.com>
2024-07-29 17:04:17 +0900

    fix: client url log 추가

--- src/main/java/com/cloudy/server/config/CorsMvcConfig.java
+++ src/main/java/com/cloudy/server/config/CorsMvcConfig.java
@@ -1,10 +1,12 @@
 package com.cloudy.server.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
 
@@ -16,11 +18,12 @@ public class CorsMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry corsRegistry) {
+		log.info("client URL is... {}", clientUrl);
 		corsRegistry.addMapping("/**")
 			.allowCredentials(true)
 			.allowedHeaders("*")
 			.exposedHeaders("Set-Cookie")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-			.allowedOrigins(clientUrl, testUrl);
+			.allowedOrigins("https://cloudy.cloudy.n-e.kr", testUrl);
 	}
 }
